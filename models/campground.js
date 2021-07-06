64b22005b50011eb8587b372296f2285f06c1ac7
Yash Agrawal <yashagrawal81930@gmail.com>
2021-07-06 11:28:04 +0530

    added review model

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,12 @@ const CampgroundSchema = new Schema({
     price: Number,
     image: String,
     description: String,
-    location: String
+    location: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
