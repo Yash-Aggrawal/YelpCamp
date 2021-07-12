800f7dc3e5c8745f7b1ed72ec9ba33943b4595f0
Yash Agrawal <yashagrawal81930@gmail.com>
2021-07-12 12:13:25 +0530

    added flash messages

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError')
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 
@@ -27,6 +29,18 @@ db.once('open', function () {
     console.log('We are Connected to database');
 
 });
+const sessionConfig = {
+    secret: 'thisisasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -34,7 +48,11 @@ app.listen(3000, () => {
     console.log('listening on port 3000');
 })
 
-
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('index', {
@@ -49,8 +67,10 @@ app.get('/campgrounds/new', (req, res) => {
 
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a Campground');
     res.redirect(`/campgrounds/${campground._id}`)
 
 
@@ -59,6 +79,10 @@ app.post('/campgrounds', catchAsync(async (req, res, next) => {
 
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds');
+    }
 
     res.render('edit', {
         campground
@@ -69,6 +93,10 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
     //  console.log(campground);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds');
+    }
     res.render('show', {
         campground
     });
@@ -80,12 +108,14 @@ app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(req.params.id, {
         ...req.body.campground
     })
+    req.flash('success', 'Successfully updated the campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
+    req.flash('success', 'Successfully deleted the campground');
     res.redirect('/campgrounds');
 }))
 
@@ -95,6 +125,7 @@ app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Created new review');
     res.redirect(`/campgrounds/${campground._id}`);
 
 }))
@@ -110,6 +141,7 @@ app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => 
         }
     })
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted the review');
     res.redirect(`/campgrounds/${id}`);
 
 }))
